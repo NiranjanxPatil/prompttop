8fa1043e9bfb5c249f3dea4048b30ca3dae20632
Niranjan <niranjan41210@gamail.com>
2023-09-07 00:54:27 +0530

    Initial commit

---
+++ components/Feed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const feed = () => {
+  return (
+    <div>feed</div>
+  )
+}
+
+export default feed
---
+++ components/PeomptCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PeomptCard = () => {
+  return (
+    <div>PeomptCard</div>
+  )
+}
+
+export default PeomptCard
