8e8b5a1d97908a95c533560bfbba5f70c19c05db
Niranjan <niranjan41210@gamail.com>
2023-09-08 19:58:11 +0530

    tried with env

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         </h1>
         <p className='desc text-center'>
           Promptopia is an open-source AI prompting tool for the modern world to
-          discover, create, and share creative prompts
+          discover, create, and share creative promptsss
         </p>
         <Feed />
       </section>
