662d79607b755df7dee27caf91fe580c21aa3d80
Niranjan <141761808+NiranjanxPatil@users.noreply.github.com>
2024-01-07 16:52:14 +0530

    Error in beam

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,33 +1,32 @@
+// Importing necessary styles and components
 import "@styles/globals.css";
-
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import Particles from "@components/particles";
 import { AppFooter } from "@components/Footer";
 
+// Metadata for the page
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
+// RootLayout component
 const RootLayout = ({ children }) => (
   <html lang='en'>
-    <body
-		<script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="cea78beb-0056-47f4-8382-00f1ed721ce1" async></script>		
-			>
-        
+    <head>
+      {/* Adding script for beam analytics */}
+      <script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="cea78beb-0056-47f4-8382-00f1ed721ce1" async></script>
+    </head>
+    <body>
       <Provider>
-        <div className='main '>
+        <div className='main'>
           <div className='gradient' />
         </div>
-        <main className='app '>
-          
+        <main className='app'>
           <Nav />
-          
           {children}
-
           <AppFooter/>
-
         </main>
       </Provider>
     </body>
